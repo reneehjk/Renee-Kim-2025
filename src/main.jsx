ac9f7923b7fa29e3ccd0461074313942acb4a352
Renee Kim <reneekim@Renees-MacBook-Air-2.local>
2025-12-25 07:53:17 -0500

    Adding analytics and speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "./index.css";
 
 import NavBar from "./components/NavBar";
@@ -10,6 +12,8 @@ import Projects from "./pages/Projects.jsx";
 import About from "./pages/About.jsx";
 import Contact from "./pages/Contact.jsx";
 import AppLayout from "./Layout";
+import ScrollToTop from "./components/ScrollToTop";
+
 
 import Plotd from "./pages/projects/Plotd.jsx";
 import Kimchi from "./pages/projects/Kimchi.jsx";
@@ -18,21 +22,25 @@ import Qux from "./pages/projects/Qux.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      
-        <NavBar />
-        <AppLayout>
+      <NavBar />
+
+      <AppLayout>
+        <ScrollToTop />
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
-            <Route path="/projects/plotd" element={<Plotd />} />
-            <Route path="/projects/kimchi-studios" element={<Kimchi />} />
-            <Route path="/projects/qux" element={<Qux />} />
+          <Route path="/projects/plotd" element={<Plotd />} />
+          <Route path="/projects/kimchi-studios" element={<Kimchi />} />
+          <Route path="/projects/qux" element={<Qux />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-        </AppLayout>
-        <Footer />
-      
+      </AppLayout>
+
+      <Footer />
+      <Analytics />
+      <SpeedInsights />
     </BrowserRouter>
   </StrictMode>
 );
